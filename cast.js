ebe828ec9b1ac4c5264c7b1a57b636ded1fc73a3
Alex Umrysh <aumrysh@gmail.com>
2016-07-21 06:03:40 -0900

    Added dynamic local ip address acquisition through interface selection. TODO: add traversal of all network interfaces for bonjour advertisement

--- cast.js
+++ cast.js
@@ -4,11 +4,23 @@ var myArgs = require('optimist').argv;
 var Client = require("castv2-client").Client;
 var DefaultMediaReceiver = require("castv2-client").DefaultMediaReceiver;
 var args = process.argv;
-var ipaddr = os.networkInterfaces()["eth0"][0]["address"]
+//var ipaddr = os.networkInterfaces()["eth0"][0]["address"]
+var interfaces = os.networkInterfaces();
 
 var client = new Client();
 var hosts = [];
 var hostname = myArgs.c;
+
+//Determine network interfaces, take the first external ipv4
+for (var nif in interfaces) {
+  console.log(nif);
+  if (interfaces[nif][0].internal === false) {
+    localip = interfaces[nif][0].address;
+    break;
+  }
+}
+
+console.log(localip);
 
 bonjour.find({type: 'googlecast'}, function(svc){
   if (svc.name.toUpperCase() === hostname.toUpperCase()) {
@@ -17,13 +29,12 @@ bonjour.find({type: 'googlecast'}, function(svc){
 });
 function ondeviceup(host) {
 
-//var host = "192.168.1.21";
 client.connect(host, function(){
   console.log("connected, launching stream");
 
   client.launch(DefaultMediaReceiver, function(err, player) {
     var media = {
-      contentId: "http://"+ipaddr+":8099/cast-rtsp.h264",
+      contentId: "http://"+localip+":8099/cast-rtsp.h264",
       contentType: 'video/vp8',
       streamType: 'LIVE',
       metadata : {
--- runit.sh
+++ runit.sh
@@ -2,5 +2,5 @@
 trap "killall phantomjs; killall nodejs; killall ffserver; killall ffmpeg" SIGINT SIGTERM
 ffserver -f ./ffserver.conf &
 phantomjs phantomrun.js $2 | ffmpeg -y -r 30 -c:v png -f image2pipe -i - -c:v libvpx -r 30 -vf scale=1080:720 http://127.0.0.1:8099/feed1.ffm &
-nodejs cast.js $1
+nodejs cast.js -c "$1"
 
